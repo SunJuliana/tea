405a847cf31073566a549160b086b769db3e223f
SunJuliana <juliakulisheva@yandex.ru>
2022-08-08 07:25:51 +0700

    Created all first Views to demonstration app

---
+++ app/src/main/java/com/aina/tea/AddonsActivity.kt
@@ -0,0 +1,11 @@
+package com.aina.tea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddonsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addons)
+    }
+}
